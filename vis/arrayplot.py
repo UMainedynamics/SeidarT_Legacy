5da83171e71c308c2f6206719bf8f970c7288ae6
steven bernsen <stevenbernsen@stevens-MacBook-Air.local>
2020-05-23 21:19:22 -0700

    Array plot for 2 and 2.5D

--- vis/arrayplot.py
+++ vis/arrayplot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Create a reciever array and plot the timeseries
+# Create a receiver array and plot the timeseries
 
 import numpy as np
 import glob
@@ -13,28 +13,46 @@ from scipy.io import FortranFile
 
 # -------------------------- Command Line Arguments ---------------------------
 parser = argparse.ArgumentParser(description="""This program creates an equally
-	spaced array of recievers given the x, y, and z coordinates. If the model 
+	spaced array of receivers given the x, y, and z coordinates. If the model 
 	isn't specified as 2.5d in the project file then """ )
 
-parser.add_argument( 'meta_file', nargs=1, type=str, 
-						help='the file path for the survey metadata', 
-						default=None)
+parser.add_argument( 
+	'-p', '--prjfile', 
+	nargs = 1, type = str, required = True,
+	help = 'The project file path.'
+)
 
-parser.add_argument( '-c', '--channel', nargs = 1, type = str, required = True,
+parser.add_argument( 
+	'-r', '--rcxfile', 
+	nargs=1, type=str, required = True, 
+	help='the file path for the text file of receiver locations'
+)
+
+parser.add_argument( 
+	'-c', '--channel', 
+	nargs = 1, type = str, required = True,
 	help = """The channel to query. """)
 
-parser.add_argument('-g', '--gain', nargs = 1, type = float, required = False,
+parser.add_argument(
+	'-g', '--gain', 
+	nargs = 1, type = float, required = False,
 	help = "The exponential value for 2^m of the gain function (default=None)", default = None)
 
-parser.add_argument('-L', '--layout', nargs = 1, type = int, required = False,
-	help = """Plot the reciever layout (1/0). This suppresses generating the 
+parser.add_argument(
+	'-L', '--layout', 
+	nargs = 1, type = int, required = False,
+	help = """Plot the receiver layout (1/0). This suppresses generating the 
 	t-x timeseries plot.""", default = [0])
 
-parser.add_argument('-S', '--suppress_plotting', nargs = 1, type = int, 
-	help = """Suppress all plotting (1/0). The reciever outputs will be located in 
-	the reciever_array.csv file.""", required = False, default = [0])
+parser.add_argument(
+	'-S', '--suppress_plotting', 
+	nargs = 1, type = int, 
+	help = """Suppress all plotting (1/0). The receiver outputs will be located in 
+	the receiver_array.csv file.""", required = False, default = [0])
 
-parser.add_argument('-e', '--exaggeration', nargs=1, type = float, required = False,
+parser.add_argument(
+	'-e', '--exaggeration', 
+	nargs=1, type = float, required = False,
 	help = """Set the aspect ratio between the x and y axes for 
 	plotting. Default is 0.5""", default = [0.5])
 
@@ -58,9 +76,9 @@ class Array:
 		self.source_location = None # Must be m-by-2 array in indices [x_ind, y_ind]
 		self.initial_position = None
 		self.final_position = None
-		self.reciever_locations = None
+		self.receiver_locations = None
 		self.modelfile = None
-		self.dr = None
+		# self.dr = None
 		self.timeseries = None
 		self.t = None
 		self.channel = None
@@ -68,6 +86,7 @@ class Array:
 		self.nx = None
 		self.ny = None
 		self.nz = None
+		self.dim = None
 
 	# -------------------------- Function Definitions -------------------------
 	def getrcx(self):
@@ -77,22 +96,38 @@ class Array:
 		all_files.sort()
 
 		m = len(all_files)
-		n = len(self.reciever_locations[:,1])
+		n = len(self.receiver_locations[:,1])
 
 		self.timeseries = np.zeros([m,n])
 
 		i = 0
-		for fn in all_files:
-			npdat = read_dat(fn, self.nx, self.nz)
-			for j in range(0, n):
-				pass
-				# Don't forget x is columns and z is rows
-				self.timeseries[i,j] = npdat[ self.reciever_locations[j,1], self.reciever_locations[j,0] ]	
+		if self.dim == '2':
+			self.ny = None
+		
+		if self.dim == '2.5':
+			for fn in all_files:
+				npdat = self.read_dat(fn)
+				for j in range(0, n):
+					# Don't forget x is columns and z is rows
+					self.timeseries[i,j] = npdat[ 
+						int(self.receiver_locations[j,2]),
+						int(self.receiver_locations[j,1]),
+						int(self.receiver_locations[j,0])
+					]
+		else:
+			for fn in all_files:
+				npdat = self.read_dat(fn)
+				for j in range(0, n):
+					# Don't forget x is columns and z is rows
+					self.timeseries[i,j] = npdat[ 
+						int(self.receiver_locations[j,2]), 
+						int(self.receiver_locations[j,0]) 
+					]	
 			
 			i = i + 1
 
 		# Save the array as csv for other types of processing
-		np.savetxt("reciever_array.csv", self.timeseries, delimiter = ",")
+		np.savetxt("receiver_array.csv", self.timeseries, delimiter = ",")
 
 	# -------------------------------------------------------------------------
 	def tsplot(self):
@@ -114,7 +149,7 @@ class Array:
 		# Label the x-axis
 		ax.xaxis.tick_top()
 		ax.xaxis.set_label_position('top')
-		ax.set_xlabel(r"Reciever #")
+		ax.set_xlabel(r"Receiver #")
 		
 		# Label the y-axis
 		if self.channel == 'Vx' or self.channel == 'Vz':
@@ -137,31 +172,33 @@ class Array:
 		plt.show()
 
 	# -------------------------------------------------------------------------
-	def arraybuild(self):
+	# def arraybuild(self):
 		
-		# Calculate the length
-		rcx_len = np.sqrt( sum( (self.final_position - self.initial_position)**2) )
+	# 	# Calculate the length
+	# 	rcx_len = np.sqrt( sum( (self.final_position - self.initial_position)**2) )
 		
-		if rcx_len == 0:
-			rxc_len = 1 # This is for common offset
-			nrcx = 1
-			xz = np.zeros([nrcx, 2])
-		else:
-			nrcx = np.floor(rcx_len/self.dr)
-			xz = np.zeros([nrcx.astype(int),2])
+	# 	# the receiver spacing must be the same as the 
+
+	# 	if rcx_len == 0:
+	# 		rxc_len = 1 # This is for common offset
+	# 		nrcx = 1
+	# 		xz = np.zeros([nrcx, 2])
+	# 	else:
+	# 		nrcx = np.floor(rcx_len/self.dr)
+	# 		xz = np.zeros([nrcx.astype(int),2])
 		
 		 
 
-		xz[:,0] = np.linspace(self.initial_position[0], 
-			self.final_position[0], nrcx)/self.dx
-		xz[:,1] = np.linspace(self.initial_position[2], 
-			self.final_position[2], nrcx)/self.dz
+	# 	xz[:,0] = np.linspace(self.initial_position[0], 
+	# 		self.final_position[0], nrcx)/self.dx
+	# 	xz[:,1] = np.linspace(self.initial_position[2], 
+	# 		self.final_position[2], nrcx)/self.dz
 
-		self.reciever_locations = xz.astype(int) + self.cpml
+	# 	self.receiver_locations = xz.astype(int) + self.cpml
 
 	# -------------------------------------------------------------------------
 	def plot_layout(self):
-
+		# Right now this only works for 2D
 		figmod, axmod = plt.subplots()
 
 		img = mpimg.imread(self.modelfile)
@@ -169,9 +206,9 @@ class Array:
 
 		implot = axmod.imshow(img, extent = extent)
 
-		# add white upside down triangle recievers
-		axmod.scatter(self.reciever_locations[:,0]- self.cpml, 
-			self.reciever_locations[:,1] - self.cpml, 
+		# add white upside down triangle receivers
+		axmod.scatter(self.receiver_locations[:,0]- self.cpml, 
+			self.receiver_locations[:,1] - self.cpml, 
 			marker = 'v', s = 30, c = (0.8, 0.8, 0.8, 1), 
 		    linewidths = 0.5, edgecolor = (0.2, 0.2, 0.2, 1 ) )
 
@@ -185,15 +222,38 @@ class Array:
 
 		plt.show()
 
-# ------------- One more quick global definition
+	# ------------- One more quick global definition
 
-def read_dat(fn, nx, ny):
-	f = FortranFile(fn, 'r')
-	dat = f.read_reals(dtype = 'float32')
-	dat = dat.reshape(ny, nx)
-	f.close()
+	def read_dat(self, fn):
+		if self.ny:
+			if self.channel == 'Ex':
+				NX = self.nz
+				NY = ny
+				NZ = nx-1
+			elif self.channel == 'Ey':
+				NX = self.nz
+				NY = self.ny-1
+				NZ = self.nx
+			elif self.channel == 'Ez':
+				NX = self.nz-1
+				NY = self.ny
+				NZ = self.nx
+			else:
+				NX = self.nz
+				NY = self.ny 
+				NZ = self.nx 
+		
+		f = FortranFile(fn, 'r')
+		dat = f.read_reals(dtype = 'float32')
+		
+		if self.ny:
+			dat = dat.reshape(NX, NY, NZ)
+		else:
+			dat = dat.reshape(self.nx, self.nz)
 
-	return(dat)
+		f.close()
+
+		return(dat)
 
 
 # ==================== Create the object and assign inputs ====================
@@ -203,7 +263,8 @@ array = Array()
 
 # Get the arguments
 args = parser.parse_args()
-meta_file = ''.join(args.meta_file)
+project_file = ''.join(args.prjfile)
+receiver_file = ''.join(args.rcxfile)
 array.channel = ''.join(args.channel)
 
 
@@ -215,27 +276,23 @@ exaggeration = args.exaggeration[0]
 
 # --------------------- Get the values from the meta file ---------------------
 
-project_file = meta_file.split('.')
-project_file = '.'.join( project_file[:-3] ) + '.prj'
+# f = open(meta_file)
 
-f = open(meta_file)
+# # If running the wrapper functions, the meta file will save the same each time,
+# # but for whatever reason we'll assume that this isn't the case
+# for line in f:
+# 	temp = line.rstrip().rsplit()
+	
+# 	if temp[0] == 'delta:':
+# 		array.dr = float(temp[1])
+	
+# 	if temp[0] == 'initial_position:':
+# 		array.initial_position = np.asarray(temp[1:]).astype(float)
+	
+# 	if temp[0] == 'final_position:':
+# 		array.final_position = np.asarray( temp[1:]).astype(float)
 
-# If running the wrapper functions, the meta file will save the same each time,
-# but for whatever reason we'll assume that this isn't the case
-for line in f:
-
-	 temp = line.rstrip().rsplit()
-
-	 if temp[0] == 'delta:':
-	 	array.dr = float(temp[1])
-	 
-	 if temp[0] == 'initial_position:':
-	 	array.initial_position = np.asarray(temp[1:]).astype(float)
-
-	 if temp[0] == 'final_position:':
-	 	array.final_position = np.asarray( temp[1:]).astype(float)
-
-f.close()
+# f.close()
 
 
 # --------------------------- Query the project file --------------------------
@@ -249,43 +306,51 @@ for line in f:
 	# Get the image file
 	if line[0] == 'I':
 		# There's a trailing new line value
-		array.modelfile = line[2:-1]
+		imfile = line[2:-1]
 
 	# All domain inputs must be input except for nz and dy
 	if line[0] == 'D':
 		temp = line.split(',')
 
+		if temp[1] == 'dim':
+			array.dim = str( temp[2].rsplit()[0] )
 		if temp[1] == 'nx':
 			array.nx = int( temp[2].rsplit()[0] )
+		if temp[1] == 'ny':
+			array.ny = int( temp[2].rsplit()[0] )
 		if temp[1] == 'nz':
 			array.nz = int( temp[2].rsplit()[0] )
 		if temp[1] == 'dx':
 			array.dx = float(temp[2].rsplit()[0] )
+		if temp[1] == 'dy':
+			array.dy = float(temp[2].rsplit()[0] )
 		if temp[1] == 'dz':
 			array.dz = float(temp[2].rsplit()[0] )
 		if temp[1] == 'cpml':
 			array.cpml = int( temp[2].rsplit()[0])
-		if temp[1] == 'write':
-			array.write = int(temp[2].rsplit()[0])
 
 	# Source
 	
-	if array.channel == 'Ex' or array.channel == 'Ez':
+	if array.channel == 'Ex' or array.channel == 'Ey' or array.channel == 'Ez':
 		if line[0] == 'E':
 			temp = line.split(',')
 			if temp[1] == 'dt':
-				edt = float(temp[2].rsplit()[0])
+				array.dt = float(temp[2].rsplit()[0])
 			if temp[1] == 'x':
 				ex = float(temp[2].rsplit()[0])
+			if temp[1] == 'y':
+				ey = float(temp[2].rsplit()[0])
 			if temp[1] == 'z':
 				ez = float(temp[2].rsplit()[0])
 	else:
 		if line[0] == 'S':
 			temp = line.split(',')
 			if temp[1] == 'dt':
-				sdt = float(temp[2].rsplit()[0])
+				array.dt = float(temp[2].rsplit()[0])
 			if temp[1] == 'x':
 				sx = float(temp[2].rsplit()[0])
+			if temp[1] == 'y':
+				sy = float(temp[2].rsplit()[0])
 			if temp[1] == 'z':
 				sz = float(temp[2].rsplit()[0])
 				
@@ -293,29 +358,46 @@ for line in f:
 f.close()
 
 # ----------------------------- Create the object -----------------------------
-
+xyz = np.genfromtxt(receiver_file, delimiter = ',', names = True, dtype = float)
+array.receiver_locations = np.vstack(
+	[
+		xyz['X']/array.dx + array.cpml,
+		xyz['Y']/array.dy + array.cpml,
+		xyz['Z']/array.dz + array.cpml
+	]
+).T
 
 # Adjust the object fields relative to the cpml
 array.nx = array.nx + 2*array.cpml
+array.ny = array.ny + 2*array.cpml
 array.nz = array.nz + 2*array.cpml
-array.dx = array.dx
-array.dz = array.dz
+# array.dx = array.dx
+# array.dy = array.dy
+# array.dz = array.dz
 
 
 extent = (array.cpml, array.nx-array.cpml, array.nz-array.cpml, array.cpml)
-if array.channel == 'Ex' or array.channel == 'Ez':
-	array.source_location = np.array([ (ex + array.cpml)/array.dx, 
-		(ez + array.cpml)/array.dz])
-	array.dt = edt
+if array.channel == 'Ex' or array.channel == 'Ey' or array.channel == 'Ez':
+	array.source_location = np.array(
+		[ 
+			(ex + array.cpml)/array.dx, 
+			(ey + array.cpml)/array.dy,
+			(ez + array.cpml)/array.dz
+		]
+	)
 else:
-	array.source_location = np.array([(sx + array.cpml)/array.dx,
-		(sz + array.cpml)/array.dz]) 
-	array.dt = sdt
+	array.source_location = np.array(
+		[
+			(sx + array.cpml)/array.dx,
+			(sy + array.cpml)/array.dy,
+			(sz + array.cpml)/array.dz
+		]
+	) 
 
-# Construct the array
-array.arraybuild()
+# # Construct the array
+# array.arraybuild()
 
-# Get the timeseries for each reciever
+# Get the timeseries for each receiver
 array.getrcx()
 
 if showplots:
