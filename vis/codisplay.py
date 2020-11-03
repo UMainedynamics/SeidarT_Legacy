897d3869cec6b78e61786f901397267ae7c7a1ab
Ian Nesbitt <ian.nesbitt@gmail.com>
2020-11-03 11:46:26 -0500

    adding try/except for Qt4 import in codisplay

--- vis/codisplay.py
+++ vis/codisplay.py
@@ -9,7 +9,12 @@
 
 # Set matplotlib backend 
 import matplotlib 
-matplotlib.use('Qt4Agg')
+try:
+    matplotlib.use('Qt4Agg')
+except Exception as e:
+    matplotlib.use('TkAgg')
+    print('Qt4 import error. Now using Tk instead of Qt. Details:')
+    print(e)
 
 import numpy as np
 import argparse
