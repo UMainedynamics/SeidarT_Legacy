d335d330103b8017a9293b8715e43f662d58aa3f
Christopher Gerbi <christopher.gerbi@maine.edu>
2020-11-10 14:23:04 -0500

    Updated help, removed need for -r and -t flags, removed printing Qt error

--- vis/codisplay.py
+++ vis/codisplay.py
@@ -3,25 +3,27 @@
 # Plot the common offset survey
 #
 # Available backends are:
-# ['GTK3Agg', 'GTK3Cairo', 'MacOSX', 'nbAgg', 'Qt4Agg', 'Qt4Cairo', 'Qt5Agg', 
-# 'Qt5Cairo', 'TkAgg', 'TkCairo', 'WebAgg', 'WX', 'WXAgg', 'WXCairo', 'agg', 
+# ['GTK3Agg', 'GTK3Cairo', 'MacOSX', 'nbAgg', 'Qt4Agg', 'Qt4Cairo', 'Qt5Agg',
+# 'Qt5Cairo', 'TkAgg', 'TkCairo', 'WebAgg', 'WX', 'WXAgg', 'WXCairo', 'agg',
 # 'cairo', 'pdf', 'pgf', 'ps', 'svg', 'template']
 
-# Set matplotlib backend 
-import matplotlib 
-matplotlib.use('Qt4Agg')
+# Set matplotlib backend
+import matplotlib
+try:
+    matplotlib.use('Qt4Agg')
+except Exception as e:
+    matplotlib.use('TkAgg')
 
 import numpy as np
 import argparse
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-from class_definitions import * 
+from class_definitions import *
 
 # -------------------------- Command Line Arguments ---------------------------
 parser = argparse.ArgumentParser(
-    description="""CODISPLAY reads the common offset or midpoint survey file 
-    created from common_offset.sh. The """ )
+    description="""Plots a csv file of timeseries data (columns) for targeted receivers """ )
 
 parser.add_argument(
     '-p', '--prjfile',
@@ -32,42 +34,40 @@ parser.add_argument(
 parser.add_argument(
     '-f', '--file',
     nargs=1, type=str, required=True,
-    help="""You have a csv file generated from the that you want plotted? Call 
-    it here."""
+    help="""Path to the csv file with receiver timeseries data"""
 )
 
 parser.add_argument(
     '-g', '--gain',
-    nargs = 1, type = float, required = False, default = 1e20,
-    help = "The exponential value for 2^m of the gain function (default=None)"
+    nargs = 1, type = float, required = False, default = 100,
+    help = "The smoothing length"
 )
 
 parser.add_argument(
     '-e', '--exaggeration',
     nargs=1, type = float, required = False, default = [0.5],
-	help = """Set the aspect ratio between the x and y axes for 
+	help = """Set the aspect ratio between the x and y axes for
 	plotting. Default is 0.5"""
  )
 
-parser.add_argument(
-    '-r', '--receiver',
-    nargs = 1, type = str, required = True,
-    help = """The csv file path that contains the receiver locations"""
-)
+#parser.add_argument(
+#    '-r', '--receiver',
+#    nargs = 1, type = str, required = True,
+#    help = """The csv file path that contains the receiver locations"""
+#)
 parser.add_argument(
     '-s', '--seismic',
     nargs = 1, type = int, required = False, default = [1],
-    help = """Flag whether this is a seismic or electromagnetic model
-    (1 - seismic, 0 - electromagnetic, default = 1)"""
+    help = """Flag whether this is a seismic or electromagnetic model"""
 )
 
-parser.add_argument(
-    '-t', '--surveytype',
-    nargs = 1, type = str, required = True,
-    help = """The type of survey that you are plotting. The available options 
-    are: war, co, cmp. The correspond to wide angle, common offset, and common
-    midpoint, respectively."""
-)
+#parser.add_argument(
+#    '-t', '--surveytype',
+#    nargs = 1, type = str, required = True,
+#    help = """The type of survey that you are plotting. The available options
+#    are: war, co, cmp. The correspond to wide angle, common offset, and common
+#    midpoint, respectively."""
+#)
 
 
 args = parser.parse_args()
@@ -76,13 +76,13 @@ cofile = ''.join(args.file)
 gain = args.gain[0]
 exaggeration = args.exaggeration[0]
 seismic = args.seismic[0] == 1
-survey_type = ''.join(args.surveytype)
+#survey_type = ''.join(args.surveytype)
 
 
 # project_file = 'easy_greenland.prj'
 # cofile = 'receiver_array.csv'
-# gain = 101 
-# exaggeration = 0.25 
+# gain = 101
+# exaggeration = 0.25
 # seismic = True
 # -----------------------------------------------------------------------------
 # Load the values from the project file
@@ -104,7 +104,7 @@ else:
     mult = 1e6
 
 timelocs = np.arange(0, m, int(m/10) ) # 10 tick marks along y-axis
-rcxlocs = np.arange(0, n, int(n/5) ) # 5 tick marks along x-axis 
+rcxlocs = np.arange(0, n, int(n/5) ) # 5 tick marks along x-axis
 
 if seismic:
     timevals = np.round(timelocs*float(seismic.dt[0]) * mult, 2)
@@ -124,7 +124,7 @@ else:
 
 fig = plt.figure(figsize =(n/2,m/2) )
 ax1 = plt.gca()
-# ax2 = ax1.twinx() 
+# ax2 = ax1.twinx()
 ax1.imshow(dat, cmap = 'Greys', aspect = 'auto')
 ax1.set_xlabel(r'Receiver #')
 ax1.xaxis.tick_top()
@@ -139,10 +139,9 @@ ax1.set_yticklabels(timevals)
 ax1.set_aspect(aspect = exaggeration)
 
 if seismic:
-	ax1.text(0, m + 0.03*m, 'x $10^{-2}$')	
+	ax1.text(0, m + 0.03*m, 'x $10^{-2}$')
 else:
 	ax1.text(0, m + 0.03*m, 'x $10^{-6}$')
 
 ax1.update_datalim( ((0,0),(m, n)))
 plt.show()
-
