49be8d07d34175ec4aa98425bed7bd1b652cb3c7
Ian Nesbitt <ian.nesbitt@gmail.com>
2021-02-10 17:59:12 -0500

    starting work on docs PDF creation config

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -21,9 +21,9 @@ from datetime import datetime
 
 # -- Project information -----------------------------------------------------
 
-project = 'SeidarT'
-copyright = '%s, Steven Bernsen' % datetime.now().year
-author = 'Steven Bernsen and Christopher Gerbi'
+project = u'SeidarT'
+copyright = u'%s, Steven Bernsen' % datetime.now().year
+author = u'Steven Bernsen, Christopher Gerbi, Ian Nesbitt'
 
 # The short X.Y version
 # the version can be set here or imported from somewhere else
@@ -50,6 +50,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.autosectionlabel',
     'sphinx.ext.githubpages',
+    'rst2pdf.pdfbuilder',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -114,7 +115,6 @@ html_static_path = ['_static']
 # 'searchbox.html']``.
 #
 # html_sidebars = {}
-
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
@@ -208,3 +208,98 @@ intersphinx_mapping = {
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
+
+
+# -- Options for PDF output --------------------------------------------------
+
+# Grouping the document tree into PDF files. List of tuples
+# (source start file, target name, title, author, options).
+#
+# If there is more than one author, separate them with \\.
+# For example: r'Guido van Rossum\\Fred L. Drake, Jr., editor'
+#
+# The options element is a dictionary that lets you override
+# this config per-document. For example:
+#
+# ('index', 'MyProject', 'My Project', 'Author Name', {'pdf_compressed': True})
+#
+# would mean that specific document would be compressed
+# regardless of the global 'pdf_compressed' setting.
+
+pdf_documents = [(
+    'index',                                        # master rst document
+    u'%s_manual' % project,                         # name of the generated pdf
+    u'%s %s Documentation' % (project, version),    # title of the pdf
+    u'%s' % author.replace(', ', '\\'),             # author names in the pdf
+),]
+
+# A comma-separated list of custom stylesheets. Example:
+pdf_stylesheets = ['sphinx', 'kerning', 'a4']
+
+# A list of folders to search for stylesheets. Example:
+pdf_style_path = ['.', '_styles']
+
+# Create a compressed PDF
+# Use True/False or 1/0
+# Example: compressed=True
+# pdf_compressed = False
+
+# A colon-separated list of folders to search for fonts. Example:
+# pdf_font_path = ['/usr/share/fonts', '/usr/share/texmf-dist/fonts/']
+
+# Language to be used for hyphenation support
+# pdf_language = "en_US"
+
+# Mode for literal blocks wider than the frame. Can be
+# overflow, shrink or truncate
+# pdf_fit_mode = "shrink"
+
+# Section level that forces a break page.
+# For example: 1 means top-level sections start in a new page
+# 0 means disabled
+# pdf_break_level = 0
+
+# When a section starts in a new page, force it to be 'even', 'odd',
+# or just use 'any'
+# pdf_breakside = 'any'
+
+# Insert footnotes where they are defined instead of
+# at the end.
+# pdf_inline_footnotes = True
+
+# verbosity level. 0 1 or 2
+# pdf_verbosity = 0
+
+# If false, no index is generated.
+# pdf_use_index = True
+
+# If false, no modindex is generated.
+# pdf_use_modindex = True
+
+# If false, no coverpage is generated.
+# pdf_use_coverpage = True
+
+# Name of the cover page template to use
+# pdf_cover_template = 'sphinxcover.tmpl'
+
+# Documents to append as an appendix to all manuals.
+# pdf_appendices = []
+
+# Enable experimental feature to split table cells. Use it
+# if you get "DelayedTable too big" errors
+# pdf_splittables = False
+
+# Set the default DPI for images
+# pdf_default_dpi = 72
+
+# Enable rst2pdf extension modules
+# pdf_extensions = []
+
+# Page template name for "regular" pages
+# pdf_page_template = 'cutePage'
+
+# Show Table Of Contents at the beginning?
+# pdf_use_toc = True
+
+# How many levels deep should the table of contents be?
+# pdf_toc_depth = 9999
